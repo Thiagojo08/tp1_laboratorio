685b0af72f86f0842a1068eba196130eac09bdc2
Thiagojo08 <thiagotavarez015@gmail.com>
2023-08-29 17:40:57 -0300

    Initial commit

---
+++ src/tp1_lab/Tp1_lab.java
@@ -0,0 +1,15 @@
+
+package tp1_lab;
+
+
+public class Tp1_lab {
+
+   
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hola como estamos");
+        
+    }
+    
+}
